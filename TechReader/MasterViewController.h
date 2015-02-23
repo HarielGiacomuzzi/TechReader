28214d379fb5a84adbeb0541875c752e9dab0f4f
Hariel Giacomuzzi <hariel.dias@acad.pucrs.br>
2015-02-23 16:02:16 -0300

    First Commit

---
+++ TechReader/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  TechReader
+//
+//  Created by Hariel Giacomuzzi on 2/23/15.
+//  Copyright (c) 2015 Hariel Giacomuzzi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
